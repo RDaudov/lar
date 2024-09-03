0dcac069862e17f54a20f8c55a84fa92b7e45538
RDaudov <rdaudov007@gmail.com>
2024-09-03 20:41:40 +0300

    in

---
+++ app/Http/Controllers/MyCont.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyCont extends Controller
+{
+    public function myTest() {
+        return 'testsss';
+    }
+}
---
+++ app/Http/Controllers/SomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SomeController extends Controller
+{
+    public function sss ( ) {
+        return 1223;
+    }
+}
